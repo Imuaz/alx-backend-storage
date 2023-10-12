90bd8e4a3d327bcb12ace9d28aa6c2f87ea7d1f2
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-12 14:03:19 +0100

    task 1 users table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- creates a table users
 CREATE TABLE IF NOT EXISTS users (
-       id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-       email VARCHAR(255) UNIQUE NOT NULL,
-       name VARCHAR(255)
-       country VARCHAR(255) NOT NULL DEFAULT 'US'
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 )
