30c8f09f22d11cc15d57c5f0e8646f8315724c85
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-12 15:20:27 +0100

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- script that lists all bands with Glam rock as
 
 SELECT band_name, COALESCE(split, 2022) - formed AS lifespan
-FROM metal_bands
-WHERE main_style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+     FROM metal_bands
+     WHERE main_style LIKE '%Glam rock%'
+     ORDER BY lifespan DESC;
