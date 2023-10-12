d029d8b61764b4fc13db2c6922327ebdfa21901e
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-12 15:12:44 +0100

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,11 @@
 -- script that lists all bands with Glam rock as
 -- their main style, ranked by their longevity
 
-SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
-    FROM bands
-    WHERE style = 'Glam rock'
-    ORDER BY lifespan DESC;
+WITH longevity AS (
+  SELECT band_name, split - formed AS lifespan
+  FROM metal_bands
+  WHERE main_style = 'Glam rock'
+)
+SELECT band_name, lifespan
+FROM longevity
+ORDER BY lifespan DESC;
