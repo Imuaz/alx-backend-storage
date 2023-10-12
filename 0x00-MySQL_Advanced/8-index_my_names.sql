a4a6308739588f212303a97c46f06a41344eedcd
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-12 20:08:47 +0100

    an Index created

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,2 @@
---  script that creates an index on the table k`names` and the first letter of `name`
--- Add a new column to store the first letter of the name
-ALTER TABLE names
-ADD COLUMN first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED;
-
-CREATE INDEX idx_name_first ON names (first_letter);
+-- script that creates an index on the table k`names` and the first letter of `name`
+CREATE INDEX idx_name_first ON names (name(1));
