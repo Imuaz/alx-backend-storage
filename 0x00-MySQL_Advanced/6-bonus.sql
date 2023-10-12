2c652c3d40ffc3a714bbd794b293901c2180e1f4
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-12 19:05:23 +0100

    stored procedure created

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,26 +1,15 @@
 -- script that creates a stored procedure AddBonus
 -- that adds a new correction for a student.
 DELIMITER //
-CREATE PROCEDURE AddBonus (
-  IN user_id INT,
-  IN project_name VARCHAR(255),
-  IN score INT
-)
-AS
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score DECIMAL(10, 2))
 BEGIN
-  -- Check if the project exists
-  IF NOT EXISTS (SELECT 1 FROM projects WHERE name = project_name)
-  THEN
-    -- Create the project
-    INSERT INTO projects (name) VALUES (project_name);
-  END IF;
+    DECLARE project_id INT;
+    SELECT id INTO project_id FROM projects WHERE name = project_name LIMIT 1;
 
-  -- Get the project ID
-  DECLARE project_id INT;
-  SET project_id = (SELECT id FROM projects WHERE name = project_name);
-
-  -- Insert the new correction
-  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-END;
-//
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END //
 DELIMITER;
