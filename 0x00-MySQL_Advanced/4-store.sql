9490510d1cfb4aeb9940c8e8dd4b8b075e00dc86
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-12 17:36:29 +0100

    Old school band

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,8 +2,7 @@
 -- the quantity of an item after adding a new order.
 
 CREATE TRIGGER update_item_quantity
-AFTER INSERT ON orders
-FOR EACH ROW
-UPDATE items
+BEFORE INSERT ON orders
+FOR EACH ROW UPDATE items
 SET quantity = quantity - NEW.quantity
 WHERE items.id = NEW.item_id;
