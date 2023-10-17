2260df29be971b9bb3e549a699541f34a1cf1c93
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-17 20:43:25 +0100

    python way of updating documents

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ MongoDB module
 
 def update_topics(mongo_collection, name, topics):
     '''changes all topics of a chool document based on the name'''
-    mongo_collection.update_many({"name":name}, {$set: {"topics": topics}})
+    mongo_collection.update_many({"name":name}, {"$set": {"topics": topics}})
