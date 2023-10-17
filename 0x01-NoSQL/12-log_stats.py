d9ec6a3dbd2bca76937b4b2287b88f68d6792925
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-17 21:37:00 +0100

    Nginx error logs

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,38 +1,28 @@
 #!/usr/bin/env python3
-''' module for task 12 '''
-import os
+'''
+MongoDB Module
+'''
 from pymongo import MongoClient
 
 
-def connect_to_mongodb():
-    connection_string = os.getenv('MONGODB_CONNECTION_STRING')
-    if connection_string is None:
-        raise ValueError("MONGODB_CONNECTION_STRING environment variable not set")
-    
-    client = MongoClient(connection_string)
-    return client.logs.nginx
-
-def get_unique_http_methods(collection):
-    return collection.distinct('method')
-
-def count_documents_with_method(collection, method):
-    return collection.count_documents({'method': method})
+def connect_to_mongodb(host='127.0.0.1', port=27017, database='logs', collection='nginx'):
+    '''connects to the db'''
+    client = MongoClient(f'mongodb://{host}:{port}')
+    return client[database][collection]
 
 def main():
     ''' script that provides some stats about Nginx logs '''
-    collection = connect_to_mongodb()
+    lst = connect_to_mongodb()
     
-    method_list = get_unique_http_methods(collection)
-
-    print("{} logs\nMethods:".format(collection.estimated_document_count()))
-
-    for method in method_list:
-        method_count = count_documents_with_method(collection, method)
-        print("\tmethod {}: {}".format(method, method_count))
-
-    status_check_count = collection.count_documents({'method': 'GET', 'path': "/status"})
-    print("{} status check".format(status_check_count))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {method: lst.count_documents({'method': method}) for method in methods}
+    
+    print(f"{lst.estimated_document_count()} logs\nMethods:")
+    for method, count in method_counts.items():
+        print(f"\tmethod {method}: {count}")
+    
+    status_check_count = lst.count_documents({'method': 'GET', 'path': "/status"})
+    print(f"{status_check_count} status check")
 
 if __name__ == "__main__":
     main()
-
