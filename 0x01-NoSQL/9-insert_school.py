7b554efbd655c4feea7c5d1842e738e112db5102
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-17 20:20:42 +0100

    python way of insertin into documents

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+MongoDB module
+'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''inserts a document in a collection based on kwargs'''
+    return mongo_collection.insert(kwargs).insert_id
